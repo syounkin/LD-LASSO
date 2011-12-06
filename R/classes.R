09d2be7107aa5225dfdd55e8352f95e33d61f796
Samuel Younkin <syounkin@jhsph.edu>
2011-12-06 16:09:00 -0500

    Added ldlasso class, generic and method
    Updated/Added two files, classes.R and ldlasso.R.  Here I define the ldlasso class, along with the generic and method to solve ldlasso regression.  Next we should add validity checking.

--- R/classes.R
+++ R/classes.R
@@ -1,1 +1,1 @@
-setClass("ldlasso", representation( geno = "numeric" ), prototype = prototype( geno = 0 ) )
+setClass("ldlasso", representation( geno = "matrix", pheno = "vector", s1 = "numeric", s2 = "numeric", r2 = "numeric", delta = "numeric" ), prototype = list( delta = 1e-6 )  )
