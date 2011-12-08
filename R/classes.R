ce398db286e6ccb04e596db5be21a8359d61ea02
Samuel Younkin <syounkin@jhsph.edu>
2011-12-08 14:18:29 -0500

    Added ldlasso class validities and ldlassoSolution class definition
    Here I defined the ldlassoSolution class as an class inherited from ldlasso which contains a slot for the solution, beta.  I also defined some validities for the ldlasso class, namely: s1, s2 need to be greater than zero, delta has to be between 0 and 1e-6, all entries in geno must be 0, 1, or 2, and all entries in pheno must be 0 or 1.

--- R/classes.R
+++ R/classes.R
@@ -1,1 +1,17 @@
-setClass("ldlasso", representation( geno = "matrix", pheno = "vector", s1 = "numeric", s2 = "numeric", r2 = "numeric", delta = "numeric" ), prototype = list( delta = 1e-6 )  )
+setClass("ldlasso", representation = representation( geno = "matrix", pheno = "numeric", s1 = "numeric", s2 = "numeric", r2 = "numeric", delta = "numeric" ), validity = function( object ) {
+  if( object@s1 < 0 )
+    return("s1 is less than zero")
+if( object@s2 < 0 )
+    return("s2 is less than zero")
+if( object@r2 < 0 )
+    return("r2 is less than zero")
+if( object@delta < 0 | object@delta > 1e-6)
+    return("delta is either negative or too large (>1e-6)")
+if( !all(geno %in% 0:2 ) )
+    return("Some genotype is not in (0, 1, 2)")
+if( !all(pheno %in% 0:1 ) )
+    return("Some phenotype is not in (0, 1)")
+})
+
+
+setClass("ldlassoSolution", contains = "ldlasso", representation = representation( beta = "numeric" ) )
