7c3eb62897f29e2d273225fe991b2e8ec011c527
Samuel Younkin <syounkin@jhsph.edu>
2012-01-28 13:15:06 -0500

    Created findS1 method and generic.
    I had trouble getting this to work, and it seems that the missing piece was the generic function.  The conclusion is that there must be a generic function associated with the method.  I should check this assumption out.  The findS1 method returns the whole ldlasso object with the new s1 value and the solution, bete, nulled out.

--- R/findS1.ldlasso.R
+++ R/findS1.ldlasso.R
@@ -1,13 +1,21 @@
-findS1 <- function( ldlasso.obj, B = 1e1, alpha = 0.05, tol = 1e-2 ){
-  cat( paste( "Null value for s1. Finding s1 for alpha = ", alpha, "...\n\n", sep = "" ) )
-  s1.low <- 0; s1.hi <- 10; fp.rate <- 1;  
+setGeneric("findS1", function(object, ... ) standardGeneric("findS1"))
+
+setMethod("findS1", "ldlasso", function( object, ... ) {
+  fn.findS1(object, ... )
+})
+
+fn.findS1 <- function( ldlasso.obj, B  = 10, alpha = 0.05, tol = 5e-2 ){
+  cat( paste( "Null value for s1. Finding s1 for alpha = ", alpha, "...\n", sep = "" ) )
+  ldlasso.obj@s1 <- NULL
+  s1.low <- 0; s1.hi <- 10; fp.rate <- 1;
+ 
   while( abs( fp.rate - alpha ) > tol ){
     fp.tot <- 0
     s1 <- mean(c(s1.low,s1.hi))
     ldlasso.obj@s1 <- s1
     for( i in 1:B ){
       ldlasso.obj@pheno <- ldlasso.obj@pheno[sample(length(ldlasso.obj@pheno))]
-      ldlasso.obj@beta <- solve(ldlasso.obj)
+      ldlasso.obj@beta <- ldlassoSolve(ldlasso.obj)
       fp <- sum( abs( ldlasso.obj@beta ) > 1e-6 )
       fp.tot <- fp + fp.tot
     }
@@ -21,5 +29,7 @@ findS1 <- function( ldlasso.obj, B = 1e1, alpha = 0.05, tol = 1e-2 ){
       s1.hi <- s1
     }
   }
-  return(ldlasso.obj@s1)
+  ldlasso.obj@s1 <- s1
+  ldlasso.obj@beta <- numeric()
+  return(ldlasso.obj)
 }
