f7a63ef2899244f621f8e207ba819d4d1bebaf3e
Samuel Younkin <syounkin@jhsph.edu>
2012-01-30 15:24:48 -0500

    Updated demo file

--- demo/ldlasso-demo.R
+++ demo/ldlasso-demo.R
@@ -1,4 +1,8 @@
-n.sub <- 1000; n.snp <- 50; s1 = 1; s2 = 1;
+n.sub <- 1000; n.snp <- 50;
 geno <- matrix( sample( 0:2, n.sub*n.snp, replace = TRUE ), nrow = n.sub, ncol = n.snp )
 pheno <- sample( 0:1, n.sub, replace = TRUE )
-ldlasso.obj <- ldlasso( geno = geno, pheno = pheno, s1 = s1, s2 = s2, r2 = 0 )
+
+ldlasso.obj <- ldlasso( geno = geno, pheno = pheno, s1 = NULL, s2 = 1, r2 = 0 )
+ldlasso.obj <- findS1(ldlasso.obj)
+
+ldlasso.obj
