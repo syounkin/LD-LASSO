423683b2a7c46b0b33050e509c2d8eac0a128222
Samuel Younkin <syounkin@jhsph.edu>
2012-01-30 13:36:51 -0500

    Update demo file

--- demo/ldlasso-demo.R
+++ demo/ldlasso-demo.R
@@ -1,5 +1,3 @@
-library("ldlasso")
-source("~/jhsph/R/reload.R")
 n.sub <- 1000; n.snp <- 50; s1 = 1; s2 = 1;
 geno <- matrix( sample( 0:2, n.sub*n.snp, replace = TRUE ), nrow = n.sub, ncol = n.snp )
 pheno <- sample( 0:1, n.sub, replace = TRUE )
