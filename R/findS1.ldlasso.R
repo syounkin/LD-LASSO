e378acdd8ebab6e8f7253959ecbb2a5150d0e037
Samuel Younkin <syounkin@jhsph.edu>
2012-02-01 10:27:16 -0500

    Added setS1 and verbose logical arguments to findS1 method.

--- R/findS1.ldlasso.R
+++ R/findS1.ldlasso.R
@@ -6,8 +6,8 @@ setMethod("findS1", "ldlasso", function( object, ... ) {
   fn.findS1(object, ... )
 })
 
-fn.findS1 <- function( ldlasso.obj, B  = 10, alpha = 0.05, tol = 5e-3 ){
-  cat( paste( "Null value for s1. Finding s1 for alpha = ", alpha, "...\n", sep = "" ) )
+fn.findS1 <- function( ldlasso.obj, B  = 10, alpha = 0.05, tol = 5e-3, setS1 = TRUE, verbose = TRUE ){
+  if(verbose) cat( paste( "Null value for s1. Finding s1 for alpha = ", alpha, "...\n", sep = "" ) )
 
   s1.low <- 0; s1.hi <- 10; fp.rate <- 1;
 
@@ -33,5 +33,6 @@ fn.findS1 <- function( ldlasso.obj, B  = 10, alpha = 0.05, tol = 5e-3 ){
   }
   ldlasso.obj@s1 <- s1
   ldlasso.obj <- solve(ldlasso.obj)
+  if( !setS1 ) ldlasso.obj@s1 <- NULL
   return(ldlasso.obj)
 }
