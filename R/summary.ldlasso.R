ebf641aaf454805998cfbc925bb7c1c5d4c2413e
Samuel Younkin <syounkin@jhsph.edu>
2012-02-02 14:48:43 -0500

    Created summary method
    The summary method does nothing right now.  All it does is execute the show method.

--- R/summary.ldlasso.R
+++ R/summary.ldlasso.R
@@ -1,10 +1,11 @@
 setMethod("summary", "ldlasso",
-# place holder for ldlasso summary method
-function( object, ... ){
-  baf <- colSums(object@geno)/2/nrow(object@geno)
-  sum.list <- list( baf = baf )
-  cat(paste("Number of subjects: ", length(object@pheno), "\n", sep = ""))
-  cat(paste("Number of SNPs: ", ncol(object@geno), "\n", sep = "" ))
-  cat("\n")
-  print(sum.list)
-})
+function( object ){
+  show( object )
+}
+)
+
+getOR <- function( object ){
+
+  
+
+}
