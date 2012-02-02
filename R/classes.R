1dbcd8d504e9620a30381fcaa8ec0db89db3c081
Samuel Younkin <syounkin@jhsph.edu>
2012-02-02 13:30:44 -0500

    Added slots for maf and variance to class ldlasso

--- R/classes.R
+++ R/classes.R
@@ -1,6 +1,20 @@
 setClassUnion("numericOrNULL", c("numeric", "NULL"))
 
-setClass( "ldlasso", representation( geno = "matrix", pheno = "integer", s1 = "numericOrNULL", s2 = "numeric", r2 = "numeric", beta = "numeric", OR = "numeric", logOR.norm = "numeric" ) )
+setClass( "ldlasso", representation(
+                                    geno = "matrix",
+                                    pheno = "integer",
+                                    s1 = "numericOrNULL",
+                                    s2 = "numeric",
+                                    r2 = "numeric",
+                                    beta = "numeric",
+                                    OR = "numeric",
+                                    logOR.norm = "numeric",
+                                    var = "numeric",
+                                    maf.case = "numeric",
+                                    maf.con = "numeric",
+                                    maf.tot = "numeric"
+                                    )
+         )
 
 ldlasso =
 # an ldlasso object with genotype matrix (subjects by SNPs) and phenotype vector
@@ -15,7 +29,7 @@ function( geno, pheno, s1, s2, r2 )
      colnames(geno) <- paste( "SNP", 1:ncol(geno), sep = "" )
   if(is.null(rownames(geno)))
      rownames(geno) <- paste( "sub", 1:nrow(geno), sep = "" )
-  new( "ldlasso", geno = geno, pheno = pheno, s1 = s1, s2 = s2, r2 = r2, OR = logOR.obj$OR, logOR.norm = logOR.obj$y )
+  new( "ldlasso", geno = geno, pheno = pheno, s1 = s1, s2 = s2, r2 = r2, OR = logOR.obj$OR, logOR.norm = logOR.obj$y, var = logOR.obj$var_y, maf.case = logOR.obj$f2, maf.con = logOR.obj$f1, maf.tot = logOR.obj$f0 )
 }
 
 
