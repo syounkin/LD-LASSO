96890d1d9b9ae34eaeff6a6d8d2507c099df3f40
Samuel Younkin <syounkin@jhsph.edu>
2012-02-02 10:58:56 -0500

    Added slots OR and logOR.norm to ldlasso class.
    Function logOR calculates OR, log OR, maf (all, cases, controls), and var(logOR).  So we simply execute logOR during creation of ldlasso object.

--- R/classes.R
+++ R/classes.R
@@ -1,6 +1,6 @@
 setClassUnion("numericOrNULL", c("numeric", "NULL"))
 
-setClass( "ldlasso", representation( geno = "matrix", pheno = "integer", s1 = "numericOrNULL", s2 = "numeric", r2 = "numeric", beta = "numeric" ) )
+setClass( "ldlasso", representation( geno = "matrix", pheno = "integer", s1 = "numericOrNULL", s2 = "numeric", r2 = "numeric", beta = "numeric", OR = "numeric", logOR.norm = "numeric" ) )
 
 ldlasso =
 # an ldlasso object with genotype matrix (subjects by SNPs) and phenotype vector
@@ -8,13 +8,14 @@ function( geno, pheno, s1, s2, r2 )
 {
   geno = as( geno, "matrix" )
   pheno = as( pheno, "integer" )
+  logOR.obj <- logOR( geno, pheno )
   if( nrow( geno) != length(pheno) )
     stop( "The number of rows in geno should equal the length of pheno" )
   if(is.null(colnames(geno)))
      colnames(geno) <- paste( "SNP", 1:ncol(geno), sep = "" )
   if(is.null(rownames(geno)))
      rownames(geno) <- paste( "sub", 1:nrow(geno), sep = "" )
-  new( "ldlasso", geno = geno, pheno = pheno, s1 = s1, s2 = s2, r2 = r2 )
+  new( "ldlasso", geno = geno, pheno = pheno, s1 = s1, s2 = s2, r2 = r2, OR = logOR.obj$OR, logOR.norm = logOR.obj$y )
 }
 
 
