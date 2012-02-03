264c112398f5a393a0776be131ce65e7ba0f9672
Samuel Younkin <syounkin@jhsph.edu>
2012-02-03 15:57:03 -0500

    Removed LD LASSO constraint form validity.R and put it in ldlassoSolve.
    I could not figure out why the LD LASSO constraint kept being violated.  So I moved it to ldlassoSolve and so far, so good.  I should keep my eye on it.  It owuld be better in validity, but I just could not get it to work there

--- R/ldlassoSolve.R
+++ R/ldlassoSolve.R
@@ -1,15 +1,12 @@
 ldlassoSolve <- function( ldlasso.obj ){
-
   if( is.null(ldlasso.obj@s1) )
     return( "Cannot solve!  The parameter s1 is NULL" )
-
   geno <- ldlasso.obj@geno
   pheno <- ldlasso.obj@pheno
   s1 <- ldlasso.obj@s1
   s2 <- ldlasso.obj@s2
   r2 <- ldlasso.obj@r2
   delta <- 1e-6
-
   p <- ncol(geno)
   index.mat <- which(cor(geno)^2 > r2 & lower.tri(matrix(1, p, p)), arr.ind = TRUE )
   r <- nrow(index.mat)
@@ -20,17 +17,14 @@ ldlassoSolve <- function( ldlasso.obj ){
     D[i,index.mat[i,2]] <- 1
     r2.vec <- c( r2.vec, cor(geno[,index.mat[i,1]],geno[,index.mat[i,2]])^2 )
   }
-
   X2 <- ldlasso.obj@X.case
   X1 <- ldlasso.obj@X.con
   f2 <- ldlasso.obj@maf.case
   f1 <- ldlasso.obj@maf.con
   n0 <- ldlasso.obj@n.con
   n1 <- ldlasso.obj@n.case
-  y <- ldlasso.obj@logOR.norm
- 
+  y <-  ldlasso.obj@logOR.norm
   ldlasso.const <- -s2*log(r2.vec) + delta
-
   One <- rep( 1, p )
   I <- diag(1, nrow = p)
   Zero <- rep( 0, p)
@@ -47,14 +41,13 @@ ldlassoSolve <- function( ldlasso.obj ){
   yc = c( y, ifelse( y >= 0, y, 0 ), ifelse( y <= 0, -y, 0 ) )
   A <- t(rbind( A1, A2, A3, A4, A5, A6 ))
   b0 <- c( rep( Zero, 3 ), -s1, rep( -ldlasso.const , 2 )  )
-
   qp <- solve.QP(Dmat = I_3p, dvec = yc, Amat = A, bvec = b0, meq = p, factorized = FALSE)
   result <- list(  qp = qp, A = A, r2.vec = r2.vec, b0 = b0 )
-
   ldlasso.obj@beta <- result$qp$solution[1:p]
-
-  stopifnot(validObject(ldlasso.obj))
-  
+  #object <- ldlasso.obj
+  good <- all( abs(abs(ldlasso.obj@beta[index.mat[,1]]) - abs(ldlasso.obj@beta[index.mat[,2]]) ) - ldlasso.const < 1e-8 )
+  #err <- sum(ifelse( t(A)%*%yc < b0, b0 - t(A)%*%yc, 0 ))
+  #cat( err, "\n" )
+  stopifnot(good)
   return(ldlasso.obj)
-  
-  }
+}
