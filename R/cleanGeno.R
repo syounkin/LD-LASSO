feeb8b2cf2945bf4d4366d3c48cf7a7cfa3f7d2e
Samuel Younkin <syounkin@jhsph.edu>
2012-02-03 13:01:07 -0500

    cleaned ot commented lines in cleanGeno

--- R/cleanGeno.R
+++ R/cleanGeno.R
@@ -4,9 +4,6 @@ cleanGeno <- function( ldlasso.obj, ... ){
     all(geno.vec==0)||all(geno.vec==2)
   }
   index.vec <- which(apply( Xa, 2, mono.test ))
-#  if(length(index.vec)!=0)
-#    Xa <- Xa[,-index.vec]
-
   index.mat <- which(cor(Xa)>0.99999 & !diag(ncol(Xa)), arr.ind = TRUE )
   if(!(nrow(index.mat)==0)){
     index.mat <- index.mat[diff(index.mat)>0,]
@@ -31,20 +28,3 @@ cleanGeno <- function( ldlasso.obj, ... ){
     return(ldlasso.obj)
   }
 }
-##  Y <-  ldlasso.obj@pheno
-##  n <- length(Y)
-##  remove SNPs in perfect LD -- to avoid noninvertible covariance matrix
-## y.mat <- c(); OR.mat <- c(); f0.mat <- c()
-##   n.boot <- 5e3
-##   for( i in 1:n.boot ){
-##     boot.vec <- sample( n, size = n, replace = TRUE )
-##     Xa.boot <- Xa[boot.vec,]
-##     Y.boot <- Y[boot.vec]
-##     logOR.obj <- logOR(geno = Xa.boot, pheno = Y.boot)
-##     y.mat <- rbind( y.mat, logOR.obj$y)
-##     OR.mat <- rbind( OR.mat, logOR.obj$OR )
-##     f0.mat <- rbind( f0.mat, logOR.obj$f0 )
-##   }
-## Sigma <- var(y.mat)
-## return(Sigma)
-## }
