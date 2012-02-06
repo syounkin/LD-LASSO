0bf97a6874c5026bcc78fb8c2156d7ebf24f89f7
Samuel Younkin <syounkin@jhsph.edu>
2012-02-06 13:18:18 -0500

    Changed show method to trivial (and comical) temporary form.
    The show method was behaving strangely after the incorporation of new slots to the ldlasso object.  The methos was outputting all of the new slots, making the output messy and illegible.  I changed it to a dummy show method for the time-being.

--- R/show.ldlasso.R
+++ R/show.ldlasso.R
@@ -1,17 +1,18 @@
 setMethod("show", "ldlasso",
 function(object){
+  cat( "Secret ldlasso object.\n" )
   # a place holder for the show method of ldlasso
-  cat( "s1 = ", object@s1, ", ", sep = "" )
-  cat( "s2 = ", object@s2, ", ", sep = "" )
-  cat( "r2 = ", object@r2, "\n", sep = "" )
-  cat( "\n" )
-  cat( nrow(object@geno), "subjects and", ncol(object@geno), "SNPs\n" )
-  if( length(object@beta) == 0 ){
-    cat( "\nNo solution.\n")
-  }else{
-    cat("\nIndices of selected SNPs:\n")
-    cat( which( abs(object@beta) > 1e-6 ), "\n" )
-  }
+  ## cat( "s1 = ", object@s1, ", ", sep = "" )
+  ## cat( "s2 = ", object@s2, ", ", sep = "" )
+  ## cat( "r2 = ", object@r2, "\n", sep = "" )
+  ## cat( "\n" )
+  ## cat( nrow(object@geno), "subjects and", ncol(object@geno), "SNPs\n" )
+  ## if( length(object@beta) == 0 ){
+  ##   cat( "\nNo solution.\n")
+  ## }else{
+  ##   cat("\nIndices of selected SNPs:\n")
+  ##   cat( which( abs(object@beta) > 1e-6 ), "\n" )
+  ## }
   ## cat("genotypes:\n")
   ## print(head(object@geno))
   ## if(nrow(object@geno) > 6)
